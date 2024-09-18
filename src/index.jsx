c577c463a22bdace30e85fceb7995fac6afc335f
areef44 <m.arif@feedloop.ai>
2024-09-18 17:51:40 +0700

    belajar react jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const heading = React.createElement('h1', null, 'Biodata Perusahaan');
-
-const listItem1 = React.createElement('li', null, 'Nama: Dicoding Indonesia');
-const listItem2 = React.createElement('li', null, 'Bidang: Education');
-const listItem3 = React.createElement('li', null, 'Tagline: Decode Ideas, Discover Potential.');
-
-const unorderedList = React.createElement('ul', null, [listItem1, listItem2, listItem3]);
-
-const container = React.createElement('div', null, [heading, unorderedList]);
+const element = (
+    <div>
+      <h1>Biodata Perusahaan</h1>
+      <ul>
+        <li>Nama: Dicoding Indonesia</li>
+        <li>Bidang: Education</li>
+        <li>Tagline: Decode Ideas, Discover Potential.</li>
+        <img src="./dicoding-logo.png" alt="Dicoding logo" />
+      </ul>
+    </div>
+);
 
 const root = createRoot(document.getElementById('root'));
-
-root.render(container);
+root.render(element);
